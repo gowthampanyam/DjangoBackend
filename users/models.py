30fb6bea3ed4b73b65c21a6a21f58e93b812097a
gowtham panyam <gowtham.panyam@gmail.com>
2023-11-23 11:20:01 +0530

    adding models for users.module

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=75)
+    code = models.CharField(max_length=75)
+    org_type = models.CharField(max_length=75)
+
+class Department(models.model):
+    name = models.CharField(max_length=75)
+    code = models.CharField(max_length=75)
+    organisation = models.ForeignKey(Organisation,on_delete=models.CASCADE)
+
+class Auditor(models.model):
+    organisation = models.ForeignKey(Organisation,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='auditor')
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+class Claimer(models.model):
+    organisation = models.ForeignKey(Organisation,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='auditor')
+    department = models.ForeignKey(Department,on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.TextField(max_length=500, blank=True)
+    phone = models.IntegerField
+    organisation = models.ForeignKey(Organisation,on_delete=models.CASCADE, null=True)
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        payload = json.loads(request.body)
+
+        first_name = payload.get('first_name', '')
+        last_name = payload.get('last_name', '')
+        email = payload.get('email', '')
+        password = payload.get('password', '')
+        confirm_password = payload.get('confirm_password', '')
+        organisation = payload.get('tenant', None)
+        user_type = payload.get('user_type', None)
+        org_code = payload.get('org_code', None)
+        redirect=payload.get('next', None) 
